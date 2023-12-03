db53372347ef0fc8c446c5bb5a1e3bc1cc0886b3
Philipp Geschke <pgeschke@gmail.com>
2023-12-03 16:12:14 +0100

    sleep more

--- tests/docker-run/test.sh
+++ tests/docker-run/test.sh
@@ -70,7 +70,7 @@ else
     # Only testing for the placeholder application, because we don't expect a successful installation
     # most likely due to no valid $FOUNDRYVTT_DOWNLOAD_URL
     echo -e "${OK_STRING} Testing only for reachable webserver, not for FoundryVTT installation..."
-    sleep 10
+    sleep 20
     curl -v http://localhost:8080
     if [ "$?" -ne "0" ]; then
       echo -e "${FAIL_STRING} Container webserver not reachable"
