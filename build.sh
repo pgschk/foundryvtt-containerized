577c51610bbae4f20ebe700dff8ea91134d2c48b
Philipp Geschke <pgeschke@gmail.com>
2023-12-03 15:54:00 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TAG=${1-"local"}
+
+set -e  # fail whenever a cmd fails
+if [ -z $(command -v docker) ]; then
+  echo -e "${FAIL_STRING} Docker binary not found"
+  exit 127
+fi
+
+# Run in script directory
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+
+docker buildx build -t pgschk/foundryvtt-containerized:${TAG} .
